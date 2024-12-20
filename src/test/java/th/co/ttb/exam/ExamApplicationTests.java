3f52be919fe2dd7b434e9f0a4007fd32efb2df6a
Thanapat Jarunvetprasert <thanapat_j@d1asia.co.th>
2024-12-20 11:07:16 +0700

    first commit

---
+++ src/test/java/th/co/ttb/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package th.co.ttb.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
